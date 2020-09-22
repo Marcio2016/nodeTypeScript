e207738ceea376ef9a4e7eb5b5f64f48051fbacd
marcio <marcio.ferreira@cialne.com.br>
2020-09-22 17:27:09 -0300

    Padronizando projeto

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
-app.listen(3333, () =>{
-    console.log('Server Iniciado!')
-})
+app.listen(3333, () => {
+    // eslint-disable-next-line no-console
+    console.log('Server Iniciado!');
+});
