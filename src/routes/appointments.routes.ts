16bcc154b1bad2ba0af44409182a09303359201c
marcio <marcio.ferreira@cialne.com.br>
2020-09-24 14:52:53 -0300

    Validando data

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,32 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
+interface Appointment {
+    id: string;
+    provider: string;
+    date: Date;
+}
 
-const agendamento = [];
+const agendamento: Appointment[] = [];
 // eslint-disable-next-line no-shadow
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
+    const parsedDate = startOfHour(parseISO(date));
+
+    // eslint-disable-next-line prettier/prettier
+    const findAppointmentInSameDate = agendamento.find(agenda => isEqual(parsedDate,agenda.date));
+
+    if (findAppointmentInSameDate) {
+        // eslint-disable-next-line prettier/prettier
+        return response.status(400).json({ message: 'This appointment is already booked' });
+    }
     const appointment = {
         id: uuid(),
         provider,
-        date,
+        date: parsedDate,
     };
     agendamento.push(appointment);
 
